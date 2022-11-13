e1b278109a20af31ba6379529e5e2047ba522551
black-desk <me@black-desk.cn>
2022-11-14 01:39:04 +0800

    tmp

--- pkg/lsp/3_17/gen/gen_type.go
+++ pkg/lsp/3_17/gen/gen_type.go
@@ -34,10 +34,13 @@ func genEnumerations(model *MetaModel) {
 			for _, value := range enumeration.Values {
 				tmp.AddField(
 					goEnumName(name, value.Name),
-					goEnumValue(value.Value))
+					goEnumValue(value.Value),
+				)
 			}
 		}
 
+		source.NewVar().
+			AddDecl(name + "VarlidateSlice")
 	}
 
 	fileName := "enumerations_gen.go"
