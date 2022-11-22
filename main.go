f1efff42316f26b0a6844861e80c33543186ca7c
black-desk <me@black-desk.cn>
2022-11-22 10:12:29 +0800

    tmp

--- main.go
+++ main.go
@@ -7,17 +7,4 @@ import "github.com/black-desk/notels/cmd"
 
 func main() {
 	cmd.Execute()
-	X := []string{}
-	if err := func() error {
-		for _, x := range X {
-			if "" == x {
-				return nil
-			}
-		}
-
-		return nil
-	}(); err != nil {
-		return err
-	}
-
 }
