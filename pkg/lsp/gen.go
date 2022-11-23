7e63279afe670575344e354a7dc0a17788436c53
black-desk <me@black-desk.cn>
2022-11-23 19:40:33 +0800

    chore(pkg/lsp): update metaModel.json

--- pkg/lsp/gen.go
+++ pkg/lsp/gen.go
@@ -2,14 +2,7 @@ package protocol
 
 // Download metaModel.json
 
-//go:generate wget -O metaModel.json https://raw.githubusercontent.com/microsoft/vscode-languageserver-node/f2864f8fb946ad8ff82928d5087d84210f77aa2d/protocol/metaModel.json
-
-// The original metaModel.json has some problems.
-// - https://github.com/microsoft/vscode-languageserver-node/issues/1125
-// - https://github.com/microsoft/vscode-languageserver-node/issues/1103
-// fix-metaModel.patch will fix them
-
-//go:generate patch -u metaModel.json -i fix-metaModel.patch
+//go:generate wget -O metaModel.json https://raw.githubusercontent.com/microsoft/vscode-languageserver-node/205a43a011713e8d5d668f76d1b666d40dfc1ede/protocol/metaModel.json
 
 // Generate lsp protocol types and interface
 
