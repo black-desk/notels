0b9dfd75b901fd99392fde638459e51cf2efbd58
black-desk <me@black-desk.cn>
2022-11-30 15:26:21 +0800

    feat(pkg/jsonrpc2): WIP add error handling

--- pkg/jsonrpc2/error.go
+++ pkg/jsonrpc2/error.go
@@ -1,6 +1,9 @@
 package jsonrpc2
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 type Error interface {
 	Error() string
@@ -8,15 +11,69 @@ type Error interface {
 	Data() json.RawMessage
 }
 
+type ErrorImpl struct {
+	msg  string
+	code Code
+	data json.RawMessage
+}
+
 func AnalysisError(
 	err error,
 	errorData json.RawMessage,
-        code Code,
+	code Code,
 	methodErr error,
 	paramsUnmarshal bool,
 	call bool,
 	errorDataUnmarshal bool,
 ) Error {
-        // TODO(black_desk): Not implementd yet.
-        return nil
+	/*
+	   if method == "textDocument/diagnostic" {
+	           var tmpResult *lsp.DocumentDiagnosticReport
+
+	           var tmpParams *lsp.DocumentDiagnosticParams
+	           err = json.Unmarshal(params, &tmpParams)
+	           if err != nil {
+	                   return
+	           }
+
+	           paramsUnmarshal = true
+
+	           var tmpErrorData *lsp.DiagnosticServerCancellationData
+
+	           tmpResult, tmpErrorData, code, methodErr = a.service.TextDocumentDiagnostic(
+	                   ctx,
+	                   tmpParams,
+	           )
+	           call = true
+
+	           if tmpErrorData != nil {
+	                   errorData, err = json.Marshal(tmpErrorData)
+	                   if err != nil {
+	                           return
+	                   }
+	           }
+
+	           errorDataUnmarshal = true
+	           if tmpResult != nil {
+	                   result, err = json.Marshal(tmpResult)
+	                   if err != nil {
+	                           return
+	                   }
+	           }
+	           return
+	   }
+	*/
+
+	var Err ErrorImpl
+	if err != nil {
+		if !paramsUnmarshal {
+			if syntax, ok := err.(*json.SyntaxError); ok {
+				Err.code = ParseErrorCode
+			} else if err.(*json.Error) {
+
+			}
+			Err.msg = fmt.Sprintf("%v", err)
+		}
+	}
+	return nil
 }
